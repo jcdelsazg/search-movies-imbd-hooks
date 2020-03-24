e7cbd933d794b01ab08fe8e5cca736fb29a0d740
Juan Carlos Del Saz <jcdelsazg@gmail.com>
2020-03-24 18:13:57 +0100

    Search movies app with react hooks, redux, redux saga skeleton

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import configureStore from './redux/store';
+const store = configureStore();
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store={store} />
   </React.StrictMode>,
   document.getElementById('root')
 );
