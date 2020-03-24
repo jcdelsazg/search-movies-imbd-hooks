e62ad487862caac39be2b218c44d3ef99c2f66e1
Juan Carlos Del Saz <jcdelsazg@gmail.com>
2020-03-24 18:25:09 +0100

    Implementing and styling home component

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Container,
+  Typography,
+  Card,
+  Grid,
+  TextField,
+  Button
+} from '@material-ui/core';
+
+import styles from './styles';
+import { MovieIcon } from '../../components/ui/icons';
 
 export default function Home() {
-  return <div>Home</div>;
+  const [searchText, setSearchText] = useState('');
+  const classes = styles();
+
+  const handleSearchTextChange = event => {
+    setSearchText(event.target.value);
+  };
+
+  const handleCleanSearchTextClick = event => {};
+  const handleSearchTextClick = event => {};
+
+  return (
+    <Container className={classes.container}>
+      <Card className={classes.cardContainer}>
+        <Grid container className={classes.titleGridContainer}>
+          <Grid>
+            <Typography className={classes.title}>Bienvenido!</Typography>
+          </Grid>
+          <Grid>
+            <MovieIcon className={classes.movieIcon}></MovieIcon>
+          </Grid>
+        </Grid>
+        <TextField
+          value={searchText}
+          placeholder="Buscar..."
+          onChange={handleSearchTextChange}
+          className={classes.textFieldSearch}
+        />
+        <Grid className={classes.buttonsContainer}>
+          <Button variant="contained" onClick={handleCleanSearchTextClick}>
+            Limpiar
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            onClick={handleSearchTextClick}
+            className={classes.searchButton}
+          >
+            Buscar
+          </Button>
+        </Grid>
+      </Card>
+    </Container>
+  );
 }
